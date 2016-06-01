d5ecfb76a1e0fd7c9be7c7b36683a65b2395db9d
goodsleepgorira <goodsleepgorira@yahoo.co.jp>
2016-06-01 02:06:06 -0400

    Initial Commit

---
+++ TestToolbar2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestToolbar2
+//
+//  Created by 齋藤緒 on 2016/06/01.
+//  Copyright © 2016年 TestOrganization. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
